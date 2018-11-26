517c04f5861ec4e874f74f1b30a04708fa934421
DavidKe91 <davidgkennedy@hotmail.com>
2018-11-26 21:19:13 +0000

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app" class="container">
+    <img src="./assets/logo.png" class="logo">
+    <toDoList></toDoList>
+    <todolisttest></todolisttest>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import toDoList from './components/todolist'
+import todolisttest from './components/todolisttest'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    toDoList,
+    todolisttest
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600;
+  margin: 0 auto;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 24px;
+}
+
+.logo {
+  display: block;
+  height: 75px;
+  margin: 20px auto;
+}
+</style>
---
+++ src/components/todolisttest.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <input class="todo-input" type="text" placeholder="What needs to be done" v-model="newtodo" @keyup.enter="addToArray">
+    <div class="todo-item" v-for="todo in todos" :key="todo.id">{{todo.title}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'todolisttest',
+    data () {
+      return {
+        newtodo: '',
+        idfortodotest: 3,
+        todos: [
+          {
+            'id': 1,
+            'title': 'First Title',
+            'completed': false,
+          },
+          {
+            'id': 2,
+            'title': 'Second Title',
+            'completed': false,
+          },
+        ]
+      }
+    },
+    methods: {
+      addToArray(){
+      this.todos.push({
+        id: this.idfortodotest,
+        title: this.newtodo,
+        completed: false,
+      })
+      this.idfortodotest++
+      this.newtodo = ''
+      }
+      }
+  }
+</script>
